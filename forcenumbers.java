818ecce8a63d01ec4763f575bf351d68f2ae2e18
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-20 16:13:26 +0300

    Create forcenumbers.java

---
+++ forcenumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class forcenumbers{
+    public static void main(String[] args) {
+
+        int n, k, i, total =1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter The Base Number : ");
+
+        n = input.nextInt();
+
+        System.out.print("Enter The Exponent Number : ");
+
+        k = input.nextInt();
+
+
+        for(i = 1; i <= k; i++){
+
+            total *= n;
+
+        }
+        System.out.println("Sonuç : " + total);
+    }
+}
